e979928a6fb9bbd9fb0b9b8941d0e4b494664a6d
xuanyan <xuanyan1983@gmail.com>
2013-07-03 11:24:04 +0800

    add class

---
+++ src/Cache.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of the Geek-Zoo Projects.
+ *
+ * @copyright (c) 2013 Geek-Zoo Projects More info http://www.geek-zoo.com
+ * @license http://opensource.org/licenses/gpl-2.0.php The GNU General Public License
+ * @author xuanyan <xuanyan@geek-zoo.com>
+ *
+ */
+
+class Cache
+{
+    private $cacheHandler = null;
+    private $ns = '';
+    private $lastKey = '';
+
+    function __construct($path = null)
+    {
+        if ($path) {
+            require_once dirname(__FILE__).'/fileHandler.php';
+            $this->cacheHandler = new fileHandler($path);
+        }
+    }
+
+    public function setHandler($obj)
+    {
+        $this->cacheHandler = $obj;
+    }
+
+    public function ns($key)
+    {
+        $this->cacheHandler->ns = $key;
+
+        return $this;
+    }
+
+    public function delete($key = null)
+    {
+        $this->cacheHandler->delete($key);
+        $this->cacheHandler->ns = null;
+
+        return true;
+    }
+
+    public function get($key)
+    {
+        $result = $this->cacheHandler->get($key);
+
+        $this->cacheHandler->ns = null;
+
+        return $result;
+    }
+
+    public function set($key, $value = aray(), $expire = 3600)
+    {
+        $this->cacheHandler->set($key, $value, $expire);
+        $this->cacheHandler->ns = null;
+    }
+}
+
+
+abstract class cacheHandler
+{
+    public $ns = null;
+
+    public function delete($key)
+    {
+        throw new Exception("u must rewrite the 'delete' method");
+    }
+
+    public function get($key)
+    {
+        throw new Exception("u must rewrite the 'get' method");
+    }
+
+    public function set($key, $value, $expire)
+    {
+        throw new Exception("u must rewrite the 'set' method");
+    }
+}
---
+++ src/fileHandler.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+ * This file is part of the Geek-Zoo Projects.
+ *
+ * @copyright (c) 2013 Geek-Zoo Projects More info http://www.geek-zoo.com
+ * @license http://opensource.org/licenses/gpl-2.0.php The GNU General Public License
+ * @author xuanyan <xuanyan@geek-zoo.com>
+ *
+ */
+
+class fileHandler extends cacheHandler
+{
+    private $path = null;
+    private $now = 0;
+
+    function __construct($path)
+    {
+        $this->path = $path;
+
+        $this->now = time();
+    }
+
+    private function readFile($filename)
+    {
+        if (!file_exists($filename)) {
+            return false;
+        }
+
+        $data = require $filename;
+
+        if ($data['timeout'] < $this->now) {
+            return false;
+        }
+
+        return $data['data'];
+    }
+
+    private function writeFile($filename, $data, $expire)
+    {
+        $dir = dirname($filename);
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+        }
+        
+        $data = array(
+            'timeout' => $this->now+$expire,
+            'data' => $data
+        );
+
+        $data = var_export($data, true);
+        $tmp = <<<EOT
+<?php
+return $data;
+?>
+EOT;
+
+        file_put_contents($filename, $tmp);
+    }
+
+    private function _delete($dir)
+    {
+        if (!file_exists($dir)) {
+            return false;
+        }
+
+        if (is_file($dir)) {
+            return unlink($dir);
+        }
+
+        $path = new DirectoryIterator($dir);
+        foreach ($path as $val) {
+            if (!$val->isDot()) {
+                $this->_delete($val->getPathname());
+            }
+        }
+
+        return rmdir($dir);
+    }
+
+    public function delete($key)
+    {
+        if ($this->ns) {
+            if ($key === null) {
+                $filename = $this->path.'/'.md5($this->ns);
+            } else {
+                $filename = $this->path.'/'.md5($this->ns).'/'.md5($key).'.php';
+            }
+            $this->ns = '';
+        } else {
+            $filename = $this->path.'/'.md5($key).'.php';
+        }
+
+        return $this->_delete($filename);
+    }
+
+    public function get($key)
+    {
+        if ($this->ns) {
+            $filename = $this->path.'/'.md5($this->ns).'/'.md5($key).'.php';
+            $this->ns = '';
+        } else {
+            $filename = $this->path.'/'.md5($key).'.php';
+        }
+
+        return $this->readFile($filename);
+    }
+
+    public function set($key, $value, $expire = 3600)
+    {
+        if ($this->ns) {
+            $filename = $this->path.'/'.md5($this->ns).'/'.md5($key).'.php';
+            $this->ns = '';
+        } else {
+            $filename = $this->path.'/'.md5($key).'.php';
+        }
+        $this->writeFile($filename, $value, $expire);
+
+        return true;
+    }
+}
+
+?>
